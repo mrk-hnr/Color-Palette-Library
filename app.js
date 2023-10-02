e43a1b218c1333db482bb5439d4489e0f93f6f6a
mrk-hnr <mrk.hnr@hotmail.com>
2023-10-02 11:28:42 +0800

    Created random HEX color generator function and embed HEX color ID into DOM

---
+++ app.js
@@ -0,0 +1,39 @@
+// -------------------------- GLOBAL VARIABLES/SELECTIONS -------------------------- 
+const colorDivs = document.querySelectorAll(".color")
+const generateButton = document.querySelectorAll(".generate")
+const sliders = document.querySelectorAll(`input[type="range"]`)
+const currentHex = document.querySelectorAll(".color h2")
+
+let initialColor;
+
+// -------------------------- FUNCTIONS -------------------------- 
+
+// ----------------------------------------------------------
+// Generates Colors
+function generateHex() {
+    const letters = "#0123456789ABCDEF" // Characters HEX consists of
+    let hash = "#" // HEX colors always starts with #
+    
+    for (let i = 0; i < 6; i++) { // HEX colors are 6 characters long, excluding #
+        hash += letters[Math.floor(Math.random() * 16)] //Randomly chooses 1 character from letters until it meets 6 characters long, w/c is added in hash
+    }
+    return hash
+}
+
+
+let randomHex = generateHex()
+// ----------------------------------------------------------
+
+function randomColor() {
+    colorDivs.forEach((div, index) => {
+        const hexText = div.children[0]
+        const randomColor = generateHex()
+
+        // Add color to background
+        div.style.backgroundColor = randomColor
+        hexText.innerText = randomColor
+    })
+}
+
+randomColor()
+
